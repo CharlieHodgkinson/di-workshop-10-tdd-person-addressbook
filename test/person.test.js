028115c893d5b445bb573a45bf487d60f602fc36
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-15 11:52:55 +0000

    building constructor

--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,14 @@
 const chai = require('chai')
 const expect = chai.expect
 
-// Your Person test goes here!
+const Person = require('../models/person.js')
+
+describe('Person', () => {
+  it('should initialize properly', () => {
+    let person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+
+    expect(person.firstName).to.equal('Joe')
+    expect(person.surname).to.equal('Bloggs')
+    expect(person.dob).to.equal('1 Jan 1990')
+  })
+})
