805602d29ef9eab44e1a082621bbcf97e8dd23b1
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-15 12:09:50 +0000

    task 2

--- models/person.js
+++ models/person.js
@@ -1,8 +1,17 @@
 class Person {
   constructor(firstName, surname, dob) {
-    this.firstName = firstName
+    this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1)
     this.surname = surname
     this.dob = dob
+    this.emails = []
+  }
+
+  fullname() {
+    return this.firstName + ' ' + this.surname
+  }
+
+  addEmail(email) {
+    this.emails.push(email)
   }
 }
 
--- test/person.test.js
+++ test/person.test.js
@@ -10,5 +10,25 @@ describe('Person', () => {
     expect(person.firstName).to.equal('Joe')
     expect(person.surname).to.equal('Bloggs')
     expect(person.dob).to.equal('1 Jan 1990')
+    expect(person.emails).to.deep.equal([])
   })
+
+  it('should capitalize first name', () => {
+    let person = new Person('amy', 'Smith', '1 Jan 1990')
+    expect(person.firstName).to.equal('Amy')
+  })
+
+  it('should return a full name', () => {
+    let person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+    expect(person.fullname()).to.equal('Joe Bloggs')
+  })
+
+  it('should add emails to array', () => {
+    let person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+    person.addEmail('joe@example.com')
+    person.addEmail('joe.bloggs@example.co.uk')
+
+    expect(person.emails).to.deep.equal(['joe@example.com', 'joe.bloggs@example.co.uk'])
+  })
+
 })
